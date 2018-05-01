2f9ca45aa55fa13027ca690fd69434d2070fc45a
yangxianxu <295907629@qq.com>
2018-05-02 01:26:48 +0800

    fix #21 完成职位列表展示页面

--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -1,3 +1,13 @@
 from flask import Blueprint
 
 job = Blueprint('job', __name__, url_prefix='/job')
+
+@job.route('/')
+def index():
+    page = request.args.get('page',default=1,type=int)
+    pagination = Course.query.paginate(
+        page=page,
+        per_page=current_app.config['INDEX_PER_PAGE'],
+        error_out=False
+        )
+    return render_template('job.html',pagination=pagination)
